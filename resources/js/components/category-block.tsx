5b3483ee5575283e8dd2000503848b865fd4ad6f
abhaypithadiya <abhaypithadiya@gmail.com>
2025-03-16 20:07:38 +0530

    Feat: Added swiper to categories.

--- resources/js/components/category-block.tsx
+++ resources/js/components/category-block.tsx
@@ -23,10 +23,10 @@ const CategoryBlock: React.FC<CategoryBlockProps> = ({ label, icon: Icon, select
     return (
         <div
             onClick={handleClick}
-            className={`flex cursor-pointer flex-col items-center justify-center gap-2 border-b-2 p-3 transition hover:text-neutral-800 ${selected ? 'border-b-neutral-800' : 'border-transparent'} ${selected ? 'text-neutral-800' : 'text-neutral-500'}`}
+            className={`flex cursor-pointer flex-col items-center justify-center gap-2 border-b-2 py-3 transition hover:text-neutral-800 ${selected ? 'border-b-neutral-800' : 'border-transparent hover:border-b-neutral-300'} ${selected ? 'text-neutral-800' : 'text-neutral-500'}`}
         >
-            <Icon size={26} />
-            <div className="text-sm font-medium">{label}</div>
+            <Icon size={20} />
+            <div className="text-sm font-semibold">{label}</div>
         </div>
     );
 };
