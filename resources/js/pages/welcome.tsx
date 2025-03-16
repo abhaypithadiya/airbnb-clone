5059ceadcba99447b15774616b86c9075229385e
abhaypithadiya <abhaypithadiya@gmail.com>
2025-03-16 11:14:42 +0530

    WIP: Auth layouts.

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,3 +1,4 @@
+import Modal from '@/components/modals/modal';
 import Navbar from '@/components/navbar/navbar';
 import { type SharedData } from '@/types';
 import { Head, usePage } from '@inertiajs/react';
@@ -11,6 +12,7 @@ export default function Welcome() {
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
+            <Modal title="Log in or sign up" isOpen actionLabel="Continue" />
             <Navbar />
         </>
     );
