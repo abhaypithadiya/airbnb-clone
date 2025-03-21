da63e199d17733a8fec7eb59791d72632494c21f
abhaypithadiya <abhaypithadiya@gmail.com>
2025-03-21 06:58:36 +0530

    Chore: Added migrations and models.

---
+++ app/Models/AttributeCategory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AttributeCategory extends Model
+{
+    /** @use HasFactory<\Database\Factories\AttributeCategoryFactory> */
+    use HasFactory;
+}
