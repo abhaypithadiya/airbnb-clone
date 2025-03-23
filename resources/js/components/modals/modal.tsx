dc76dad0be0b53e4deea1d40b4cfcbfbe96b53c6
abhaypithadiya <abhaypithadiya@gmail.com>
2025-03-23 16:39:14 +0530

    Feat: Added static step 2 for registration modal.

--- resources/js/components/modals/modal.tsx
+++ resources/js/components/modals/modal.tsx
@@ -31,6 +31,14 @@ const Modal: React.FC<ModalProps> = ({
 
     useEffect(() => {
         setShowModal(isOpen);
+        if (isOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = '';
+        }
+        return () => {
+            document.body.style.overflow = '';
+        };
     }, [isOpen]);
 
     const handleClose = useCallback(() => {
@@ -72,7 +80,7 @@ const Modal: React.FC<ModalProps> = ({
                         onClick={(e) => e.stopPropagation()}
                         className={`translate h-full duration-300 ${showModal ? 'translate-y-0' : 'translate-y-full'} ${showModal ? 'opacity-100' : 'opacity-0'}`}
                     >
-                        <div className="translate relative flex h-full w-full flex-col rounded-3xl border-0 bg-white shadow-lg md:h-auto lg:h-auto">
+                        <div className="translate relative flex h-full max-h-[90vh] w-full flex-col rounded-3xl border-0 bg-white shadow-lg md:h-auto lg:h-auto">
                             <div className="relative flex items-center justify-center rounded-t border-b-[1px] p-6">
                                 <button
                                     onClick={handleClose}
@@ -82,15 +90,17 @@ const Modal: React.FC<ModalProps> = ({
                                 </button>
                                 <div className="text-lg font-semibold">{title}</div>
                             </div>
-                            <div className="relative flex-auto p-6 pb-0">{body}</div>
-                            <div className="flex flex-col gap-2 p-6">
-                                <div className="flex w-full flex-row items-center gap-4">
-                                    {secondaryActionLabel && secondaryAction && (
-                                        <Button outline disabled={disabled} label={secondaryActionLabel} onClick={handleSecondaryAction} />
-                                    )}
-                                    <Button disabled={disabled} label={actionLabel} onClick={handleSubmit} />
+                            <div className="overflow-y-auto">
+                                <div className="relative flex-auto p-6 pb-0">{body}</div>
+                                <div className="flex flex-col gap-2 p-6">
+                                    <div className="flex w-full flex-row items-center gap-4">
+                                        {secondaryActionLabel && secondaryAction && (
+                                            <Button outline disabled={disabled} label={secondaryActionLabel} onClick={handleSecondaryAction} />
+                                        )}
+                                        <Button disabled={disabled} label={actionLabel} onClick={handleSubmit} />
+                                    </div>
+                                    {footer}
                                 </div>
-                                {footer}
                             </div>
                         </div>
                     </div>
--- resources/js/components/modals/register-modal.tsx
+++ resources/js/components/modals/register-modal.tsx
@@ -1,5 +1,5 @@
 import useRegisterModal from '@/hooks/use-register-modal';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BsPhone } from 'react-icons/bs';
 import { FaApple, FaFacebook } from 'react-icons/fa';
 import { FcGoogle } from 'react-icons/fc';
@@ -10,9 +10,17 @@ import Modal from './modal';
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
     const [isLoading, setIsLoading] = useState(false);
+    const [step, setStep] = useState(1);
+
+    useEffect(() => {
+        if (registerModal.isOpen) {
+            setStep(1);
+        }
+    }, [registerModal.isOpen]);
 
     function handleSubmit() {
         console.log('form is submitted');
+        setStep(2);
     }
 
     const bodyContent = (
@@ -32,16 +40,67 @@ const RegisterModal = () => {
         </div>
     );
 
+    const stepTwoBodyContent = (
+        <div className="flex flex-col gap-4">
+            <div className="text-md font-bold">Legal Name</div>
+            <div>
+                <Input id="first-name" label="First name on ID" disabled={isLoading} required />
+                <Input id="last-name" label="Last name on ID" disabled={isLoading} required />
+                <p className="mt-2 text-xs font-semibold text-neutral-500">
+                    Make sure this matches the name on your government ID. If you go by another name, you can add a{' '}
+                    <span className="cursor-pointer font-bold underline hover:text-black">preferred first name</span>.
+                </p>
+            </div>
+            <div className="text-md font-bold">Date Of Birth</div>
+            <div>
+                <Input id="date-of-birth" label="Date of Birth" type="date" disabled={isLoading} required />
+                <p className="mt-2 text-xs font-semibold text-neutral-500">
+                    To sign up, you need to be at least 18. Your birthday won’t be shared with other people who use Airbnb.
+                </p>
+            </div>
+            <div className="text-md font-bold">Contact Info</div>
+            <div>
+                <Input id="email" label="Email" type="text" disabled={isLoading} required />
+                <p className="mt-2 text-xs font-semibold text-neutral-500">We'll email you trip confirmations and receipts.</p>
+            </div>
+            <div className="text-md font-bold">Password</div>
+            <div>
+                <Input id="password" label="Password" type="password" disabled={isLoading} required />
+            </div>
+            <p className="mt-2 text-xs font-semibold text-neutral-800">
+                By selecting <span className="font-extrabold">Agree and continue</span>, I agree to Airbnb’s{' '}
+                <span className="cursor-pointer text-[#004CC4] underline">Terms of Service</span>,
+                <span className="cursor-pointer text-[#004CC4] underline">Payments Terms of Service</span>, and{' '}
+                <span className="cursor-pointer text-[#004CC4] underline">Nondiscrimination Policy</span> and acknowledge the{' '}
+                <span className="cursor-pointer text-[#004CC4] underline">Privacy Policy</span>.
+            </p>
+        </div>
+    );
+
+    const stepTwoFooterContent = (
+        <div className="mt-3 flex flex-col gap-4">
+            <hr />
+            <p className="text-xs">
+                Airbnb will send you members-only deals, inspiration, marketing emails, and push notifications. You can opt out of receiving these at
+                any time in your account settings or directly from the marketing notification.
+            </p>
+            <label htmlFor="marketing-message" className="flex cursor-pointer flex-row gap-3">
+                <input type="checkbox" name="marketing-message" id="marketing-message" className="cursor-pointer" />
+                <p className="text-xs">I don’t want to receive marketing messages from Airbnb.</p>
+            </label>
+        </div>
+    );
+
     return (
         <Modal
             disabled={isLoading}
             isOpen={registerModal.isOpen}
-            title="Log in or sign up"
-            actionLabel="Continue"
+            title={step == 1 ? 'Log in or sign up' : 'Finish signing up'}
+            actionLabel={step == 1 ? 'Continue' : 'Agree and Continue'}
             onClose={registerModal.onClose}
             onSubmit={handleSubmit}
-            body={bodyContent}
-            footer={footerContent}
+            body={step == 1 ? bodyContent : stepTwoBodyContent}
+            footer={step == 1 ? footerContent : stepTwoFooterContent}
         />
     );
 };
